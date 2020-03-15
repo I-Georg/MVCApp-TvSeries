6d03bdbab368a28522c1e1fcc219a149d2222ba1
I-Georg <47190936+I-Georg@users.noreply.github.com>
2020-03-15 17:25:03 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary;
+using static DataLibrary.BusinessLogic.TvProcessor;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewTvSeries()
+        {
+            ViewBag.Message = "TvSeries List";
+
+            var data = LoadTvSeries();
+            List<TvModel> series = new List<TvModel>();
+
+            foreach (var row in data)
+            {
+                series.Add(new TvModel
+                {
+                    TvSeriesID = row.TvSeriesID,
+                    Title = row.Title,
+                    Genre = row.Genre,
+                    Rating = row.Rating,
+                    Price = row.Price
+                    
+                });
+            }
+
+            return View(series);
+        }
+
+        public ActionResult AddSeries()
+        {
+            ViewBag.Message = "Add tv series";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddSeries(TvModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int recordsCreated = CreateTvSeries(model.TvSeriesID,model.Title,model.ChannelOrigin,model.Genre,model.Rating,model.Price
+                                                    );
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ Models/TvModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class TvModel
+    {
+        [Display(Name = "Tv Series ID")]
+        [Range (1, 999999, ErrorMessage = "You need to enter a valid employeeId")]
+        public int TvSeriesID { get; set; }
+
+        [Display(Name = "Title of series")]
+        [Required(ErrorMessage = "you need to give us a name")]
+        public string Title { get; set; }
+
+        [Display(Name = "Channel origin")]
+        [Required(ErrorMessage = "you need to give us a channel name")]
+        public string ChannelOrigin { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required(ErrorMessage = "you need to give us a genre")]
+    
+        public string Genre { get; set; }
+
+        [Display(Name = "Rating")]
+        [Required(ErrorMessage = "you need to give us a rating out of 10")]
+        [Range(1, 10, ErrorMessage = "You need to enter a rating")]
+        public int Rating { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "you need to enter the price you paid")]
+        //[Range(1, 100, ErrorMessage = "You need to enter a number")]
+        public decimal Price { get; set; }
+
+
+    }
+}
