a608adea79bd847d5c896a002ac4ae09beaf837d
I-Georg <47190936+I-Georg@users.noreply.github.com>
2020-03-19 13:26:40 +0000

    aded viewmodel

---
+++ ViewModel/TvSeriesUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.ViewModel
+{
+    public class TvSeriesUserViewModel
+    {
+        public UserModel Users { get; set; }
+
+        public List<TvModel> TvSeries { get; set; }
+    }
+}
